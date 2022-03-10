58598b2ff60e9fc216507bb4b677bea61e6a613d
takayasugiyama <s.takaya1027@gmail.com>
2022-03-10 19:00:35 +0900

    :+1: add member info scraping

--- scraping/scraping_member_info_to_csv.rb
+++ scraping/scraping_member_info_to_csv.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 require 'csv'
 require 'smarter_csv'
 
+result = []
 csvs = SmarterCSV.process("./member.csv")
 csvs.each do |csv|
   link = csv[:profile_link]
@@ -15,6 +16,34 @@ csvs.each do |csv|
 
   profile = html_doc.css('.profile_detail').first
 
+  data = {}
+
   name = profile.css('.name_j').text
-  p profile.css("dl dt").count
+  data[:name] = name
+
+  profile_doc = profile.css("dl dd")
+
+  nickname = profile_doc[0].text
+  data[:nickname] = nickname
+
+  birthday = profile_doc[1].text.scan(/(\d+)年(\d+)月(\d+)日/).flatten.map(&:to_i)
+  data[:birthday_year] = birthday[0]
+  data[:birthday_month] = birthday[1]
+  data[:birthday_day] = birthday[2]
+
+  data[:blood_type] = profile_doc[2].text
+  data[:place_of_birth] = profile_doc[3].text
+  data[:height] = profile_doc[4].text.scan(/\d+/).first.to_i
+  data[:hobby] = profile_doc[5].text
+  data[:special_skill] = profile_doc[6].text
+  data[:best_feature] = profile_doc[7].text
+  result << data
 end
+
+CSV.open("member_info.csv", "w") do |csv|
+  csv << result.first.keys
+  result.each do |r|
+    csv << r.values
+  end
+end
+
