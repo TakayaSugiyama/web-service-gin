613767e66f366d5ccf31db200b9555ffb6b0bc88
takayasugiyama <s.takaya1027@gmail.com>
2022-04-07 22:56:43 +0900

    fix datasource

--- db/config.go
+++ db/config.go
@@ -2,12 +2,21 @@ package db
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func configTestCliant() string {
+	if value, ok := os.LookupEnv("TEST_CLIENT"); ok && value == "CI" {
+		return "ginuser:password@tcp(127.0.0.1:3306)/gin-service?parseTime=true"
+	}
+	return "ginuser:password@tcp(gin_service_db:3306)/gin-service?parseTime=true"
+}
+
 func ConnectDB() *sql.DB {
-	db, err := sql.Open("mysql", "ginuser:password@tcp(gin_service_db:3306)/gin-service?parseTime=true")
+	var dnsNmae string = configTestCliant()
+	db, err := sql.Open("mysql", dnsNmae)
 	if err != nil {
 		panic(err.Error())
 	}
