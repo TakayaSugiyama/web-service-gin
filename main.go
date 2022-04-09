452571a4b4adca949c2870763d873240b783fc16
takayasugiyama <s.takaya1027@gmail.com>
2022-04-09 17:12:48 +0900

    fix env

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/TakayaSugiyama/web-service-gin/routes"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	router := routes.InitRoutes()
 	port := routes.SetPortandGinMode()
 	router.Run(":" + port)
